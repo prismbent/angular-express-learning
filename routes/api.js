99f3bcb2b33fbfc9fbf6e4356d715b42b4498cd0
Melissa Zhang <melissa@Melissa.local>
2014-04-06 09:55:37 -0700

    added character pictures

--- routes/api.js
+++ routes/api.js
@@ -16,70 +16,80 @@ exports.characters = function (req, res) {
     "house":    "Lannister",
     "actor":    "Peter Dinklage",
     "quote":    "It's not easy being drunk all the time. If it were easy, everyone would do it.",
-    "episodes": 28
+    "episodes": 28,
+    "img":      "tyrion-lannister.jpg"
   },
   {
     "name":     "Cersei Lannister",
     "house":    "Lannister",
     "actor":    "Lean Headey",
     "quote":    "Everyone who isn't us is an enemy.",
-    "episodes": 27
+    "episodes": 27,
+    "img":      "cersei-lannister.png"
   },
   {
     "name":     "Arya Stark",
     "house":    "Stark",
     "actor":    "Maisie Williams",
     "quote":    "Valar Morghulis.",
-    "episodes": 27
+    "episodes": 27,
+    "img":      "arya-stark.png"
   },
   {
     "name":     "Daenerys Targaryen",
     "house":    "Targaryen",
     "actor":    "Emilia Clarke",
     "quote":    "Where are my dragons?",
-    "episodes": 25
+    "episodes": 25,
+    "img":      "daenerys-targaryen.jpg"
   },
   {
     "name":     "Catelyn Stark",
     "house":    "Stark",
     "actor":    "Michelle Fairley",
     "quote":    "You can always say no, Ned.",
-    "episodes": 25
+    "episodes": 25,
+    "img":      "catelyn-stark.jpg"
   },
   {
     "name":     "Ned Stark",
     "house":    "Stark",
     "actor":    "Sean Bean",
     "quote":    "Winter is coming.",
-    "episodes": 11
+    "episodes": 11,
+    "img":      "ned-stark.jpg"
   },
   {
     "name":     "Hodor",
     "house":    "Stark",
     "actor":    "Kristian Nairn",
     "quote":    "Hodor.",
-    "episodes": 17
+    "episodes": 17,
+    "img":      "hodar.jpg"
   },
   {
     "name":     "Joffrey Baratheon",
     "house":    "Lannister",
     "actor":    "Jack Gleeson",
     "quote":    "I am the king! I will punish you.",
-    "episodes": 23
+    "episodes": 23,
+    "img":      "joffrey-baratheon.jpg"
   },
   {
     "name":     "Jaime Lannister",
     "house":    "Lannister",
     "actor":    "Nikolaj Coster-Waldau",
     "quote":    "I know. People have been swinging at me for years and they always seem to miss.",
-    "episodes": 19
+    "episodes": 19,
+    "img":      "jaime-lannister.png"
   },
   {
     "name":     "Robb Stark",
     "house":    "Stark",
     "actor":    "Richard Madden",
     "quote":    "Now we'll see what the words are worth.",
-    "episodes": 21
+    "episodes": 21,
+    "img":      "robb-stark.png"
   }
 
 ]
